84b66d700764a22b4dd8631b7b670a15aa956b27
canhsan <1979hasancan@gmail.com>
2023-04-01 03:47:17 +0300

    javanın 1.günü

---
+++ src/day01_javayaGiris/CO1_HelloWorld.java
@@ -0,0 +1,16 @@
+package day01_javayaGiris;
+
+public class CO1_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("java'yı sıfırdan tekrar ediyorum.");
+        // bu yorum satırıdır.
+        // Java çalışırken bunu dikkate almaz.
+        //Toplu bir şekilde yorum satırı yapmak istiyorsan /*----*/ arasına almalısın
+        /*
+        işte bu
+        toplu olarak
+        satırları
+        yoruma alama işaretidir.
+         */
+    }
+}
