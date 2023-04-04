92fa7ecb44e7021725dad5f99dab0a3c5d737837
canhsan <1979hasancan@gmail.com>
2023-04-04 04:54:04 +0300

    day03

---
+++ src/day03_DataCasting/C01_AoutoWedding.java
@@ -0,0 +1,8 @@
+package day03_DataCasting;
+
+public class C01_AoutoWedding {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/day03_DataCasting/C02_ExpilicitNarrowing.java
@@ -0,0 +1,9 @@
+package day03_DataCasting;
+
+public class C02_ExpilicitNarrowing {
+    public static void main(String[] args) {
+
+    }
+
+
+}
