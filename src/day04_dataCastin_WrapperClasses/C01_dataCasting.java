ac12e963aa59843a6d4c9987aeab48bffd83bdde
canhsan <1979hasancan@gmail.com>
2023-04-05 03:29:49 +0300

    4.gün

---
+++ src/day04_dataCastin_WrapperClasses/C01_dataCasting.java
@@ -0,0 +1,22 @@
+package day04_dataCastin_WrapperClasses;
+
+public class C01_dataCasting {
+    public static void main(String[] args) {
+
+        String str1 = "10";
+        System.out.println(Integer.valueOf(str1));
+        System.out.println(Integer.parseInt(str1));
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.valueOf(str1));
+        System.out.println(Integer.parseInt(str1) + 1);
+
+    }
+
+
+}
