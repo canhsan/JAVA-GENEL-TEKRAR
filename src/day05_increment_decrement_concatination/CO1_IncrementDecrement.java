40547712b91cb533ac1726b97320101dde75dc90
canhsan <1979hasancan@gmail.com>
2023-04-11 03:15:58 +0300

    5.gün

---
+++ src/day05_increment_decrement_concatination/CO1_IncrementDecrement.java
@@ -0,0 +1,21 @@
+package day05_increment_decrement_concatination;
+
+public class CO1_IncrementDecrement {
+    public static void main(String[] args) {
+        int sayi = 20;
+        System.out.println(sayi);
+        sayi += 3;
+        System.out.println(sayi);
+        sayi -= 10;
+        System.out.println(sayi);
+        int sayi2 = 0;
+        sayi2 = sayi;
+        System.out.println(sayi2);
+        sayi2--;
+        System.out.println(sayi2);
+        System.out.println("sayi :" + sayi +" " +"sayi2 :" + sayi2);
+
+
+
+    }
+}
