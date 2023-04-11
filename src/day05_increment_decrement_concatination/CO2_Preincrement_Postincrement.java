0c53b7309e8cffc8b6ebf0f89b66cf42476e3c53
canhsan <1979hasancan@gmail.com>
2023-04-11 04:41:16 +0300

    5.gün

---
+++ src/day05_increment_decrement_concatination/CO2_Preincrement_Postincrement.java
@@ -0,0 +1,14 @@
+package day05_increment_decrement_concatination;
+
+public class CO2_Preincrement_Postincrement {
+    public static void main(String[] args) {
+        int a=20;
+        System.out.println(a++);
+        System.out.println(++a);
+        int b=++a;
+        System.out.println(b);
+        b=a++;
+        System.out.println(b);
+
+    }
+}
