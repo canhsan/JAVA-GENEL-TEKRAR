5b0b6e6ad6e09c5889eb644d46b0d70040ccc86b
canhsan <1979hasancan@gmail.com>
2023-04-23 07:01:57 +0300

    1.23/04/2023

---
+++ src/day08_methodOlusturma/C01_method.java
@@ -0,0 +1,13 @@
+package day08_methodOlusturma;
+
+public class C01_method {
+    public static void main(String[] args) {
+        String str="Java Candir";
+        str.toUpperCase();
+        str.toLowerCase();
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+
+}
